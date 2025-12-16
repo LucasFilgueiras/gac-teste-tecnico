91ad5cadb134f9510dfd4417b366934582073b36
LucasFilgueiras <lucks_fil@hotmail.com>
2025-12-15 21:58:53 -0300

    feat: add rate limting

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
 import { StatisticsModule } from './statistics/statistics.module';
 import { HealthModule } from './health/health.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [TransactionsModule, StatisticsModule, HealthModule],
+  imports: [
+    TransactionsModule,
+    StatisticsModule,
+    HealthModule,
+    ThrottlerModule.forRoot([
+      // 10 requisições por minuto
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
